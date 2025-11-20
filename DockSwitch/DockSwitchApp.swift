43e0565f9aa89ec40d67bb8f50d912a0f14c0660
VellerRider <geadro@outlook.com>
2025-11-19 22:06:05 -0500

    Initial Commit

---
+++ DockSwitch/DockSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DockSwitchApp.swift
+//  DockSwitch
+//
+//  Created by Qihang Yang on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct DockSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DockSwitchTests/DockSwitchTests.swift
@@ -0,0 +1,17 @@
+//
+//  DockSwitchTests.swift
+//  DockSwitchTests
+//
+//  Created by Qihang Yang on 11/19/25.
+//
+
+import Testing
+@testable import DockSwitch
+
+struct DockSwitchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
